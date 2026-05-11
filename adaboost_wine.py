ee2a88a6c3c58de84aff2ac2a5595ff212b8a4b3
Krish <krishagr1451@gmail.com>
2026-05-11 19:15:34 +0530

    Remove unused imports (numpy, pandas) and clean code

--- adaboost_wine.py
+++ adaboost_wine.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pandas as pd
-
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
--- polynomial_regression_diabetes.py
+++ polynomial_regression_diabetes.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pandas as pd
-
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
